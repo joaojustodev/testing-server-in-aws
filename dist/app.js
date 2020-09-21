c4b92eeabb79823548df8818fe0e246a4bd459f4
Joaobr200 <joaoarantesjob@gmail.com>
2020-09-21 02:05:48 -0300

    add route list

--- dist/app.js
+++ dist/app.js
@@ -10,10 +10,23 @@ const app = express_1.default();
 exports.app = app;
 app.use(cors_1.default());
 app.use(express_1.default.json());
+const list = [
+    {
+        name: "Computador",
+        price: 5000.0,
+    },
+    {
+        name: "Monitor",
+        price: 1250.0,
+    },
+    {
+        name: "Nootbook",
+        price: 3000.0,
+    },
+];
 app.get("/", (req, res) => {
     res.status(200).json({ message: "OK!" });
 });
-app.get("/:name", (req, res) => {
-    const { name } = req.params;
-    res.status(201).json({ messaje: `Hellow ${name}` });
+app.get("/list", (req, res) => {
+    res.status(201).json(list);
 });
--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const list = [
+  {
+    name: "Computador",
+    price: 5000.0,
+  },
+  {
+    name: "Monitor",
+    price: 1250.0,
+  },
+  {
+    name: "Nootbook",
+    price: 3000.0,
+  },
+];
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "OK!" });
 });
 
-app.get("/:name", (req, res) => {
-  const { name } = req.params;
-
-  res.status(201).json({ messaje: `Hellow ${name}` });
+app.get("/list", (req, res) => {
+  res.status(201).json(list);
 });
 
 export { app };
