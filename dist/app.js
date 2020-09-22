85755a1581ad7ca9c16e5935232e3c6c5a08b2e1
joaobr200 <joaoarantesjob@gmail.com>
2020-09-22 20:26:45 +0000

    add new route to get a list item

--- dist/app.js
+++ dist/app.js
@@ -12,22 +12,27 @@ app.use(cors_1.default());
 app.use(express_1.default.json());
 const list = [
     {
+        id: "1",
         name: "Computador",
         price: 5000.0,
     },
     {
+        id: "2",
         name: "Monitor",
         price: 1250.0,
     },
     {
+        id: "3",
         name: "Nootbook",
         price: 3000.0,
     },
     {
+        id: "4",
         name: "Mouse gamer",
         price: 400.0,
     },
     {
+        id: "5",
         name: "Teclado gamer",
         price: "390,00"
     }
@@ -37,4 +42,9 @@ app.get("/", (req, res) => {
 });
 app.get("/list", (req, res) => {
     res.status(201).json(list);
+});
+app.get("/list/:id", (req, res) => {
+    const { id } = req.params;
+    const item = list.filter(item => item.id === id);
+    res.status(200).json(item);
 });
--- src/app.ts
+++ src/app.ts
@@ -8,22 +8,27 @@ app.use(express.json());
 
 const list = [
   {
+    id: "1",
     name: "Computador",
     price: 5000.0,
   },
   {
+    id: "2",
     name: "Monitor",
     price: 1250.0,
   },
   {
+    id: "3",
     name: "Nootbook",
     price: 3000.0,
   },
   {
+    id: "4",
     name: "Mouse gamer",
     price: 400.0,
   },
   {
+    id: "5",
     name: "Teclado gamer",
     price: "390,00"
   }
@@ -36,5 +41,12 @@ app.get("/", (req, res) => {
 app.get("/list", (req, res) => {
   res.status(201).json(list);
 });
+
+app.get("/list/:id", (req,res) => {
+    const { id } = req.params;
+    const item  = list.filter(item => item.id === id)
+  
+    res.status(200).json(item);
+})
 
 export { app };
